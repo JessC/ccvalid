c290ea6a6669a584b2b4f1b57373e746458a33dc
JessC <jesse.callwood@gmail.com>
2015-12-26 15:45:41 -0800

    working validity algorithm

--- javascripts/src/code.js
+++ javascripts/src/code.js
@@ -1,18 +1,33 @@
-var ccNumber = "4342562099462994"
+var ccNumber = "4342-5620-9946-2994"
 
 function is_valid() {
-	//this.value = this.value.replace(/[^\d]/g, '');
+	console.log(ccNumber); //show original cc number
+	ccNumber = ccNumber.replace(/[^\d]/g, ''); //remove non-numbers
 	//alert(ccNumber.length != 19 ? 'must be 19 numbers' : 'awesome');
-
-
-
-console.log(ccNumber);
-var ccNumRev = ccNumber.split("").reverse().join("");
-console.log(ccNumRev + "   Reversed!!!");
-
+var i, l, ccNumRev;            //set variables
+sum = 0;                         
+ccNumRev = ccNumber.split("").reverse().join("");//reverse string
+console.log(ccNumRev + "   Reversed!!!");        //show cc number reversed
+for (i = 0, l = ccNumRev.length; i < l; i++) {
+	num = ccNumRev[i];           //assign index to numbers
+	num = +num;
+if (i % 2) {                   //if index even, then * value by 2
+	num *= 2;					           
+	if (num < 10) {						 //adds numbers together if less than 10
+		sum += num;							 //if more than 10...add number and subtract
+	} else {									 //so individual digits are added ex:
+		sum += num - 9;					 //3+12 should be -> 3+1+2
+	} 												 //3+12-9= is the same as 3+1+2=6
+} else {
+	sum += num;
+}console.log(num);
 }
+console.log(sum % 10 === 0); 
+return sum % 10 === 0;
+};
 
- is_valid();
+
+is_valid();
 
 // Reverse the digits:
 //   61789372994
